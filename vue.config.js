cbeb5a9d70f19d6866057a5e27b65c89ff48ea1a
likaixuan <1375502718@qq.com>
2019-01-23 09:40:18 +0800

    chore: gzip 与 sourceMap

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,32 @@
 
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
 const { externals, cssCdn, jsCdn } = require('./cdnConfig.js')
 
 const isDev = process.env.NODE_ENV === 'development'
 const isProd = process.env.NODE_ENV === 'production'
+
+// 生产环境开启gzip
+const productionGzip = true
+
+let plugins = []
+
+if (isProd && productionGzip) {
+  plugins.push(
+    new CompressionWebpackPlugin({
+      test: new RegExp('\\.(' + ['js', 'css'].join('|') + ')$'),
+      threshold: 10000,
+      minRatio: 0.8
+    })
+  )
+}
+
 module.exports = {
   chainWebpack: config => {
     config
       .plugin('html')
       .tap(args => {
         // 生产环境时引入cdn
-        if(isProd) {
+        if (isProd) {
           args[0].cdn = {
             css: cssCdn,
             js: jsCdn
@@ -18,9 +35,11 @@ module.exports = {
         return args
       })
   },
-  publicPath:process.env.VUE_APP_PUBLIC_PATH, // 静态资源路径
+  publicPath: process.env.VUE_APP_PUBLIC_PATH, // 静态资源路径
+  productionSourceMap: false, // 生产环境 sourceMap
   configureWebpack: {
-    externals: isProd ? externals:[], // 生产环境时配置cdn
+    externals: isProd ? externals : [], // 生产环境时配置cdn
+    plugins,
     devServer: {
       proxy: {
         '/api': {
